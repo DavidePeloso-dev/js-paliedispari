8043e775092673b233d391950789ad60a4b25345
DavidePeloso-dev <davide.peloso.e@gmail.com>
2023-12-04 16:20:33 +0100

    even or odd program

--- script.js
+++ script.js
@@ -21,4 +21,48 @@ function palindrom(){
 
 palindrom();
 
+function generateNumber(min, max){
+    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min)
+    return randomNumber
+}
+
+
+
+function sum(numb1, numb2){
+    const result = numb1 +  numb2;
+    return result;
+}
+
+function evenOrOdd(numb){
+    if(numb % 2 == 0){
+        const message = "even";
+        return message
+    } else {
+        const message = "odd";
+        return message
+    }
+}
+
+const choose = prompt('type even or odd')
+if(choose == 'even' || choose == 'odd'){
+    const userNumber = Number(prompt('type a number from 1 to 5'));
+    if(userNumber > 5 || userNumber < 1){
+        alert('you have to type only a number from 1 to 5')
+    } else {
+        const computerNumber = generateNumber(1, 5)
+
+        console.log(computerNumber);
+        const result = sum(userNumber, computerNumber);
+        console.log(result);
+        if(evenOrOdd(result) == choose){
+            console.log(`it's ${choose}, you won!`);
+        } else {
+            console.log(`it isn't ${choose}, you lose!`);
+        };
+
+    }
+} else {
+    alert('you have to type only even or odd');
+}
+
 
